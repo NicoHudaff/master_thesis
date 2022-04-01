e0ba1e158d55d15d46fd7eefe5077eb65b0ce7ea
NicoHudaff <nico.hudaff@web.de>
2022-04-01 12:30:41 +0200

    added functionality to write the results in a postgres database

--- src/import_data.py
+++ src/import_data.py
@@ -5,19 +5,29 @@ from statsbombpy import sb
 import pandas as pd
 import ray
 from pandarallel import pandarallel
+from sqlalchemy import create_engine
+from sqlalchemy.engine.base import Engine
+import json
+from typing import Optional
 
 import warnings
 
 # don't show warnings since statsbom will warn you every time that you are using the free version
 warnings.filterwarnings("ignore")
 
+# SQL connection details are stored in the config file
+with open("./config.json") as config_file:
+    config = json.load(config_file)
 
 # initialization for parellization
 pandarallel.initialize()
 ray.init(log_to_driver=False)
 
+# Define the number of Games that should be loaded in one iteration
+CHUNKSIZE_GAMES = 10
 
-def get_available_matches(limit: int = None) -> list:
+
+def get_available_matches(limit: Optional[int] = None) -> list:
     """
     This function gathers all the game ids regarding the games that are available in the free version of statsbomb
 
@@ -26,10 +36,7 @@ def get_available_matches(limit: int = None) -> list:
     """
     # first of all gather all leagues and seasons of that leagues that have available data
     available_data = (
-        sb.competitions()
-        .groupby("competition_id")
-        .season_id.apply(list)
-        .to_dict()
+        sb.competitions().groupby("competition_id").season_id.apply(list).to_dict()
     )
 
     # loop over all available seasons to gather the game ids.
@@ -235,26 +242,95 @@ def reformat_abnormal_df(df_abnormal: pd.DataFrame) -> pd.DataFrame:
     return df[[c for c in df.columns if c not in ["end_location_y", "end_location_x"]]]
 
 
-def main() -> pd.DataFrame:
+def write_to_db(df: pd.DataFrame, engine: Engine) -> None:
+    """
+    This function formats the give DataFrame to a into the database writable df.
+    Afterwards it will be written in the database table.
+
+    Input:  df:     The DataFrame that should be saved
+            engine: The sqlalchemy engine regarding the database.
+    """
+    # filter for columns that should be saved in a dictionary
+    dff = df[
+        [
+            c
+            for c in df.columns
+            if c
+            not in [
+                "type",
+                "id",
+                "match_id",
+                "minute",
+                "period",
+                "second",
+                "team",
+                "result",
+            ]
+        ]
+    ]
+
+    # reformat the df
+    to_db = pd.DataFrame(
+        {
+            "id": df.id.values,
+            "type": df.type.values,
+            "match_id": df.match_id.values,
+            "minute": df.minute.values,
+            "period": df.period.values,
+            "second": df.second.values,
+            "team": df.team.values,
+            "result": df.result.values,
+            "content": dff.to_dict("records"),
+        }
+    )
+    to_db["content"] = to_db.content.astype(str)
+
+    # write it into the database
+    with engine.connect() as conn:
+        to_db.to_sql("raw_data", conn, schema="master_thesis", if_exists="append", index=False)
+
+
+def main() -> None:
     """
     The main function gathers information regarding the normal and anomaly soccer game
     Under the column "result" it is stored whether an anomaly has occurred or not.
-    The two seperate dfs are finally concatenated together.
+    The two seperate dfs are finally stored in the same postgres database.
 
     Output:     The final df with all information in the same format
     """
+    # create the engine to connect to the postgres Database
+    engine = create_engine(
+        f'postgresql://{config.get("user")}:{config.get("password")}@{config.get("host")}:{config.get("port")}/{config.get("database")}'
+    )
+
     # load the anomaly game and mark it as anomaly
     df_abnormal = reformat_abnormal_df(load_df_abnormal())
     df_abnormal["result"] = "anomaly"
+    df_abnormal["period"] = 2.0
 
-    # load the normal games and mark it as normal
-    res = ray.get(
-        [get_match_data.remote(id) for id in get_available_matches(limit=100)]
-    )
-    df = pd.concat(res)
-    df["result"] = "normal"
+    # write the resulting df to the database
+    write_to_db(df_abnormal, engine)
 
-    return pd.concat([df, df_abnormal]).reset_index(drop=True)
+    # load information about the ids of the games and the number of games and iterations
+    matches = get_available_matches()
+    len_matches = len(matches)
+    iterations = int(len_matches / CHUNKSIZE_GAMES) + 1
+
+    # perform for each chunk loading and saving of the games information
+    for i in range(iterations):
+        print("\r", f"{i+1}/{iterations} ({round(((i+1)/iterations)*100)}%)", end="")
+
+        res = ray.get(
+            [
+                get_match_data.remote(id)
+                for id in matches[CHUNKSIZE_GAMES * i : CHUNKSIZE_GAMES * (i + 1)]
+            ]
+        )
+        df = pd.concat(res)
+
+        df["result"] = "normal"
+
+        write_to_db(df, engine)
 
 
 if __name__ == "__main__":
